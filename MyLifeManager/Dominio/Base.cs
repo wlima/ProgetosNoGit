8c07a49d52ef30e4c792281aff523b62d3aa5c45
wlima <washingtonlslima@gmail.com>
2019-04-29 10:52:40 -0300

    Subindo o sistema para o repositóro.

---
+++ MyLifeManager/Dominio/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public abstract class Base
+    {
+
+        public int Id{ get; set; }
+        public bool Status { get; set; }
+        public string log { get; set; }
+    }
+}
---
+++ MyLifeManager/Dominio/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario:Base
+    {
+        public string Email { get; set; }
+        public string Senha{ get; set; }
+
+    }
+}
